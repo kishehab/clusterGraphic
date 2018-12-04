be24d43a496bb391cf842ac304198a0ba386091c
Khalil Shehab <khaleel87@hotmail.com>
2018-12-04 18:32:06 +0300

    convert to path function added

--- Circle.js
+++ Circle.js
@@ -1,30 +1,36 @@
-        function Circle(cx,cy,cr,color,label) {
-
+        function Circle(cx, cy, cr, color, label) {
             this.cx = cx;
             this.cy = cy;
             this.cr = cr;
             this.color = color;
             this.label = label;
-            this.circle = document.createElementNS(svgNS,'circle');
-            this.g = document.createElementNS(svgNS,"g");
+            this.circle = document.createElementNS(svgNS, 'circle');
+            this.g = document.createElementNS(svgNS, "g");
             this.label = document.createElementNS(svgNS, 'text');
             //connection nodes
             // For a circle with origin (j, k) and radius r:
             // x(t) = r cos(t) + j
             // y(t) = r sin(t) + k
-
-            this.getConnectionNodes = function(){
-
-                return connectionNodes = [  
-                            {name: 'urNode', x: this.cr*Math.cos(Math.PI/4)+this.cx, y:this.cr*Math.sin(Math.PI/4)+this.cy},
-                            {name: 'ulNode', x: this.cr*Math.cos(Math.PI*(3/4))+this.cx, y:this.cr*Math.sin(Math.PI*(3/4))+this.cy},
-                            {name: 'llNode', x: this.cr*Math.cos(Math.PI*(5/4))+this.cx, y:this.cr*Math.sin(Math.PI*(5/4))+this.cy},
-                            {name: 'lrNode', x: this.cr*Math.cos(Math.PI*(7/4))+this.cx, y:this.cr*Math.sin(Math.PI*(7/4))+this.cy}
-                                        ];
-            } 
-
-
-            this.constractCircle = function(){
+            this.getConnectionNodes = function() {
+                return connectionNodes = [{
+                    name: 'urNode',
+                    x: this.cr * Math.cos(Math.PI / 4) + this.cx,
+                    y: this.cr * Math.sin(Math.PI / 4) + this.cy
+                }, {
+                    name: 'ulNode',
+                    x: this.cr * Math.cos(Math.PI * (3 / 4)) + this.cx,
+                    y: this.cr * Math.sin(Math.PI * (3 / 4)) + this.cy
+                }, {
+                    name: 'llNode',
+                    x: this.cr * Math.cos(Math.PI * (5 / 4)) + this.cx,
+                    y: this.cr * Math.sin(Math.PI * (5 / 4)) + this.cy
+                }, {
+                    name: 'lrNode',
+                    x: this.cr * Math.cos(Math.PI * (7 / 4)) + this.cx,
+                    y: this.cr * Math.sin(Math.PI * (7 / 4)) + this.cy
+                }];
+            }
+            this.constractCircle = function() {
                 this.circle.setAttribute('cx', cx);
                 this.circle.setAttribute('cy', cy);
                 this.circle.setAttribute('r', cr);
@@ -32,78 +38,65 @@
                 this.circle.setAttribute("stroke-width", 2);
                 this.circle.setAttribute('fill', this.color);
                 this.circle.setAttribute('fill-opacity', 0.4);
-
                 this.label.setAttribute('x', cx);
                 this.label.setAttribute('y', cy);
                 this.label.setAttribute('fill', '#000');
                 this.label.setAttribute('dx', '-1.5%');
                 this.label.setAttribute('dy', '3%');
                 this.label.setAttribute('font-family', 'Arial');
-
                 this.label.textContent = label;
-
                 this.circle.addEventListener('mouseover', function(e) {
-                e.currentTarget.setAttribute('stroke-width', 3);
+                    e.currentTarget.setAttribute('stroke-width', 3);
                 });
-
                 this.circle.addEventListener('mouseleave', function(e) {
-                e.currentTarget.setAttribute('stroke-width', 2);
+                    e.currentTarget.setAttribute('stroke-width', 2);
                 });
-
+                this.path = convertToPath(this.circle);
                 this.g.appendChild(this.circle);
                 this.g.appendChild(this.label);
             }
-
-            this.getX = function(){
+            
+            this.getPath = function() {
+                return this.path.getAttribute('d');
+            }
+            this.getX = function() {
                 return this.cx;
             };
-
-            this.getY = function(){
+            this.getY = function() {
                 return this.cy;
             };
-
-            this.getR = function(){
+            this.getR = function() {
                 return this.cr;
             };
-
-            this.location = function(){
-
-            }
-
+            this.location = function() {}
             this.getCircleNode = function() {
                 this.constractCircle();
                 return this.g;
             };
-
             this.fill = function(color) {
                 this.circle.setAttribute('fill', color);
             };
-
-            this.connectToMother = function(mother){
-                    var connection = document.createElementNS(svgNS,'line');
-                    var connectionNodes = this.getConnectionNodes();
-                    var motherConnectionNodes = mother.getConnectionNodes();
-                    var a, b, distance, shortestDistance = 1000; 
-
-                    for (var i = motherConnectionNodes.length - 1; i >= 0; i--) {
-                        for (var j = connectionNodes.length - 1; j >= 0; j--) {
-                             a = motherConnectionNodes[i].x - connectionNodes[j].x;
-                             b = motherConnectionNodes[i].y - connectionNodes[j].y;
-                             distance = Math.sqrt( a*a + b*b );
-                    
-                             if(distance <= shortestDistance){
-                                shortestDistance = distance;
-                                connection.setAttribute('x1', motherConnectionNodes[i].x);
-                                connection.setAttribute('y1', motherConnectionNodes[i].y);
-                                connection.setAttribute('x2', connectionNodes[j].x);
-                                connection.setAttribute('y2', connectionNodes[j].y);
-                             }
+            this.connectToMother = function(mother) {
+                var connection = document.createElementNS(svgNS, 'line');
+                var connectionNodes = this.getConnectionNodes();
+                var motherConnectionNodes = mother.getConnectionNodes();
+                var a, b, distance, shortestDistance = 1000;
+                for (var i = motherConnectionNodes.length - 1; i >= 0; i--) {
+                    for (var j = connectionNodes.length - 1; j >= 0; j--) {
+                        a = motherConnectionNodes[i].x - connectionNodes[j].x;
+                        b = motherConnectionNodes[i].y - connectionNodes[j].y;
+                        distance = Math.sqrt(a * a + b * b);
+                        if (distance <= shortestDistance) {
+                            shortestDistance = distance;
+                            connection.setAttribute('x1', motherConnectionNodes[i].x);
+                            connection.setAttribute('y1', motherConnectionNodes[i].y);
+                            connection.setAttribute('x2', connectionNodes[j].x);
+                            connection.setAttribute('y2', connectionNodes[j].y);
                         }
                     }
-
-                    connection.setAttribute("stroke", "gray");
-                    connection.setAttribute("stroke-width", 1);
-                    return connection;
+                }
+                connection.setAttribute("stroke", "gray");
+                connection.setAttribute("stroke-width", 1);
+                return connection;
             };
-
-    }
+        }
