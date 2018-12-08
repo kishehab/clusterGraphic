53f9e9dfba9f752ac5ff6240ef25cbdd1068215a
Khalil <khaleel87@hotmail.com>
2018-12-08 22:57:00 +0300

    fixing the saizing issue

--- ClusterNode.js
+++ ClusterNode.js
@@ -75,14 +75,17 @@
 
         function createClusterNode(singles, height, width){
             var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            svg.setAttribute('width', width);
+            svg.setAttribute('height', height);
+
             this.height = height;
             this.width = width;
 
             var orginX = this.width/2;
             var orginY = this.height/2;
 
-            var motherCircleRadius = 5;
-            var margin = 3;
+            var motherCircleRadius = this.height/10;
+            var margin = this.height/15;
 
             var radiusSize = {'XLarge': (this.height/2)*0.42 , 'large': (this.height/2)*0.39, 'medium':(this.height/2)*0.35,'small':(this.height/2)*0.30};
             var colorType = {'fieldService': '#f2b318', 'inspection': '#4480ac', 'maintinance': '#92b264','wellServices':'#e8355c'};
